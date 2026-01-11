116585264fab62ac3e13bf3995cd2cbf940b2468
sylvanawanjiru <sylvanawanjru@gmail.com>
2026-01-11 14:31:15 +0300

    Completed combineUsers function and passed all tests

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+require('datejs');  
+
+function combineUsers(...args) {
+    // Step 2: Initialize return object
+    const combinedObject = {
+        users: []
+    };
+
+    // Step 3 + 4: Loop through args and merge arrays using spread operator
+    for (let arr of args) {
+        combinedObject.users = [...combinedObject.users, ...arr];
+    }
+
+    // Step 5: Get today's date using datejs
+    const today = new Date();
+    combinedObject.merge_date = today.toString("M/d/yyyy");
+
+    // Step 7: Return final object
+    return combinedObject;
+}
+
+module.exports = { combineUsers };
 
 
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
